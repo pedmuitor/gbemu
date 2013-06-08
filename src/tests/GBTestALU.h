09a5c409df7d30ceb469bd0d3e1a8a885015e5a5
pedmuitor <pedmuitor@gmail.com>
2013-06-08 11:27:53 +0200

    [NEW] Target de tests

---
+++ src/tests/GBTestALU.h
@@ -0,0 +1,13 @@
+//
+//  GBTestALU.h
+//  GBEmu
+//
+//  Created by Pedro Muiños on 08/06/13.
+//  Copyright (c) 2013 None. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GBTestALU : SenTestCase
+
+@end
---
+++ src/tests/GBTestALU.m
@@ -0,0 +1,32 @@
+//
+//  GBTestALU.m
+//  GBEmu
+//
+//  Created by Pedro Muiños on 08/06/13.
+//  Copyright (c) 2013 None. All rights reserved.
+//
+
+#import "GBTestALU.h"
+
+@implementation GBTestALU
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testALU
+{
+
+}
+
+@end
