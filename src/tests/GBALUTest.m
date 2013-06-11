ff41a16cddb90f1f9fc044f7e2d970be81b9da0d
pedmuitor <pedmuitor@gmail.com>
2013-06-11 21:11:35 +0200

    [NEW] Algunos test para ADD

--- src/tests/GBALUTest.m
+++ src/tests/GBALUTest.m
@@ -29,12 +29,73 @@
     // Run after each test method
 }
 
-- (void)test8bitOps
+- (void)testADD
 {
-    REG_A = 0x01;
-    ADD(0x01);
+    int8_t initialValues [] = {
+        0x01,
+        0x8A,
+        0xB1,
+        
+        //H Tests
+        0x07,
+        //C Tests
+        0x78
+    };
     
-    GHAssertTrue(REG_A == 0x02, @"REG_A value %#X, expected %#X", REG_A, 0x02);
+    int8_t numbersToAdd [] = {
+        0x00,
+        0x2C,
+        0xD2,
+        0x06,
+        0x69
+    };
+    
+    int8_t expectedResult [] = {
+        0x01,
+        0xB6,
+        0x83,
+        0x0D,
+        0xE1
+    };
+    
+    int8_t expectedZ [] = {
+        0,
+        0,
+        0,
+        0,
+        0
+    };
+    
+    int8_t expectedH [] = {
+        0,
+        1,
+        0,
+        1,
+        1
+    };
+    
+    int8_t expectedC [] = {
+        0,
+        0,
+        0,
+        0,
+        1
+    };
+
+    int nTests = sizeof(numbersToAdd)/sizeof(int8_t);
+    
+    for (int i = 0; i < nTests; i++) {
+        REG_A = initialValues[i];
+        GHTestLog(@"Operation %d: %#X + %#X", i, REG_A, numbersToAdd[i]);
+        ADD(numbersToAdd[i]);
+        
+        GHAssertTrue(REG_A == expectedResult[i], @"Result %#X --> expected %#X", REG_A, expectedResult[i]);
+        GHAssertTrue(ALU_FLAG_N == 0, @"N %d --> expected %d", ALU_FLAG_N, 0);
+        GHAssertTrue(ALU_FLAG_Z == expectedZ[i], @"Z %d --> expected %d", ALU_FLAG_Z, expectedZ[i]);
+        GHAssertTrue(ALU_FLAG_H == expectedH[i], @"H %d --> expected %d", ALU_FLAG_H, expectedH[i]);
+        GHAssertTrue(ALU_FLAG_C == expectedC[i], @"C %d --> expected %d", ALU_FLAG_C, expectedC[i]);
+    }
+    
 }
 
 @end
